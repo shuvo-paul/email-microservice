94864287363d56b0342ac47043e86908ed0ca681
Shuvo Paul <shuvopaul@yahoo.com>
2025-11-19 13:38:55 +0600

    feat: add MockSender type and make mockClient public

--- internal/mailer/mailer_mock.go
+++ internal/mailer/mailer_mock.go
@@ -2,11 +2,20 @@ package mailer
 
 import "github.com/stretchr/testify/mock"
 
-type mockClient struct {
+type MockClient struct {
 	mock.Mock
 }
 
-func (m *mockClient) Send(to, subject, body string) error {
+func (m *MockClient) Send(to, subject, body string) error {
+	args := m.Called(to, subject, body)
+	return args.Error(0)
+}
+
+type MockSender struct {
+	mock.Mock
+}
+
+func (m *MockSender) Send(to, subject, body string) error {
 	args := m.Called(to, subject, body)
 	return args.Error(0)
 }
