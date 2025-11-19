3f392171b6046d8cad70019c807b792d2a49ce3c
Shuvo Paul <shuvopaul@yahoo.com>
2025-11-19 13:37:15 +0600

    feat: add MockQueue constractor and use sync.Once in Close method

--- internal/queue/queue_mock.go
+++ internal/queue/queue_mock.go
@@ -1,13 +1,23 @@
 package queue
 
 import (
+	"sync"
+
 	"github.com/shuvo-paul/email-microservice/internal/models"
 	"github.com/stretchr/testify/mock"
 )
 
 type MockQueue struct {
 	mock.Mock
-	jobs chan models.EmailRequest
+	jobs      chan models.EmailRequest
+	closeOnce sync.Once
+}
+
+func NewMockQueue(jobs chan models.EmailRequest) *MockQueue {
+	return &MockQueue{
+		Mock: mock.Mock{},
+		jobs: jobs,
+	}
 }
 
 func (q *MockQueue) Enqueue(job models.EmailRequest) error {
@@ -20,8 +30,10 @@ func (q *MockQueue) Jobs() <-chan models.EmailRequest {
 }
 
 func (q *MockQueue) Close() {
+	q.closeOnce.Do(func() {
+		if q.jobs != nil {
+			close(q.jobs)
+		}
+	})
 	q.Called()
-	if q.jobs != nil {
-		close(q.jobs)
-	}
 }
