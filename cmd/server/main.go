ad6063507241fe2105902c0fa87c7c95c8a0d586
Shuvo Paul <shuvopaul@yahoo.com>
2025-12-30 15:18:44 +0600

    draft

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/shuvo-paul/email-microservice/internal/config"
 	"github.com/shuvo-paul/email-microservice/internal/handlers"
+	"github.com/shuvo-paul/email-microservice/internal/mailer"
+	"github.com/shuvo-paul/email-microservice/internal/queue"
+	"github.com/shuvo-paul/email-microservice/internal/service"
+	"github.com/shuvo-paul/email-microservice/internal/worker"
 )
 
 func main() {
+	cfg := config.Load()
+	queue := queue.NewQueue(10)
+	emailService := service.NewEmailService(queue)
+	emailHandler := handlers.NewEmailHandler(emailService)
+	sender := mailer.NewSMTPSender(
+		cfg.SMTPConfig.Host,
+		cfg.SMTPConfig.Port,
+		cfg.SMTPConfig.Username,
+		cfg.SMTPConfig.Password,
+		cfg.SMTPConfig.From,
+	)
+	wp := worker.NewWorkerPool(queue, &sender)
+	wp.Start(5)
+
 	http.HandleFunc("GET /health", handlers.HealthHandler)
-	log.Println("Server starting on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/send", emailHandler.ServeHTTP)
+
+	port := fmt.Sprintf(":%d", cfg.ServerConfig.Port)
+	log.Println("Server starting on :" + port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
